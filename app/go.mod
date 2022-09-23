3f27327b0484a7ba2c73173a280395bb08e25e9a
zenialexandre <zenialexandre123@gmail.com>
2022-09-23 16:43:43 -0300

    starting my Golang data analysis practice

--- app/go.mod
+++ app/go.mod
@@ -3,6 +3,7 @@ module app
 go 1.19
 
 require (
-	github.com/google/uuid v1.3.0 // indirect
-	github.com/maxence-charriere/go-app/v9 v9.5.1 // indirect
+	github.com/go-gota/gota v0.12.0 // indirect
+	golang.org/x/net v0.0.0-20210423184538-5f58ad60dda6 // indirect
+	gonum.org/v1/gonum v0.9.1 // indirect
 )
--- app/main.go
+++ app/main.go
@@ -1,49 +1,85 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
+	"strings"
 
-	"github.com/maxence-charriere/go-app/v9/pkg/app"
+	"github.com/go-gota/gota/dataframe"
+	"github.com/go-gota/gota/series"
 )
 
-type hello struct {
-	app.Compo
-	name string
-}
-
 func main() {
-	app.Route("/", &hello{})
-	app.Route("/hello", &hello{})
-	app.RunWhenOnBrowser()
+	var asterisk string = "*"
 
-	http.Handle("/", &app.Handler{
-		Name:        "Hello",
-		Description: "An Hello World! example",
-	})
-
-	if err := http.ListenAndServe(":8000", nil); err != nil {
-		log.Fatal(err)
-	}
+	gotaSeries()
+	fmt.Println(strings.Repeat(asterisk, 45))
+	gotaDataFrame()
+	fmt.Println(strings.Repeat(asterisk, 45))
+	gotaDataFrameStruct()
 }
 
-func (h *hello) Render() app.UI {
-	return app.Div().Body(
-		app.H1().Body(
-			app.Text("Hello, "),
-			app.If(h.name != "",
-				app.Text(h.name),
-			).Else(
-				app.Text("World!"),
-			),
-		),
-		app.P().Body(
-			app.Input().
-				Type("text").
-				Value(h.name).
-				Placeholder("What is your name?").
-				AutoFocus(true).
-				OnChange(h.ValueTo(&h.name)),
-		),
+func gotaSeries() {
+	// with a slice
+	fmt.Println(series.New([]string{"z", "y", "d", "e"}, series.String, "column"))
+
+	// with a map
+	a := map[string]series.Type{
+		"A": series.String,
+		"D": series.Bool,
+	}
+	fmt.Println(a)
+}
+
+func gotaDataFrame() {
+	df := dataframe.New(
+		series.New([]string{"a", "b", "c", "d", "e"}, series.String, "alphas"),
+		series.New([]int{5, 4, 3, 2, 1}, series.Int, "numbers"),
+		series.New([]string{"a1", "b2", "c3", "d4", "e5"}, series.String, "alnums"),
+		series.New([]bool{true, false, true, true, false}, series.Bool, "state"),
 	)
+	fmt.Println(df)
+}
+
+func gotaDataFrameStruct() {
+	type Dog struct {
+		Name       string
+		Color      string
+		Height     int
+		Vaccinated bool
+	}
+
+	dogs := []Dog{
+		{"Buster", "Black", 56, false},
+		{"Jake", "White", 61, false},
+		{"Bingo", "Brown", 50, true},
+		{"Gray", "Cream", 68, false},
+	}
+
+	dogsDf := dataframe.LoadStructs(dogs)
+	fmt.Println(dogsDf)
+	queryDogsDf(dogsDf)        // querying a DataFrame
+	queryDogsDfColumns(dogsDf) // querying columns
+}
+
+func queryDogsDf(dogsDf dataframe.DataFrame) {
+	fmt.Printf("Dimensions: ")
+	fmt.Println(dogsDf.Dims())
+	fmt.Print("Types: ")
+	fmt.Println(dogsDf.Types())
+	fmt.Print("Names: ")
+	fmt.Println(dogsDf.Names())
+	fmt.Print("Number of rows: ")
+	fmt.Println(dogsDf.Nrow())
+	fmt.Print("Number of columns: ")
+	fmt.Println(dogsDf.Ncol())
+}
+
+func queryDogsDfColumns(dogsDf dataframe.DataFrame) {
+	col := dogsDf.Col("Name") // selecting a column
+
+	fmt.Printf("Verifying column values: %v\n", col.IsNaN())
+	fmt.Printf("Getting column mean: %v\n", col.Mean())
+	fmt.Printf("Copy of the column values: %v\n", col.Copy())
+	fmt.Printf("There is a null value? %v\n", col.HasNaN())
+	fmt.Printf("Records: %v\n", col.Records())
 }
